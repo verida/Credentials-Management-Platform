8b7afdc47d6c1c0d126c1977cfe9f90ee21bbd80
Chris Were <chris@mozzler.com.au>
2020-07-10 21:20:05 +0930

    Fix: dob validation, add regex error message

--- backend/src/modules/credential/dto/issue-credential.dto.ts
+++ backend/src/modules/credential/dto/issue-credential.dto.ts
@@ -2,9 +2,11 @@ import { IsObject, IsString, Length, IsNotEmpty, Matches } from 'class-validator
 
 export class IssueCredentialDto {
     @IsString()
-    @Length(8,8)
+    @Length(10,10)
     @IsNotEmpty()
-    @Matches(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)
+    @Matches(/[0-9]{4}-[0-9]{2}-[0-9]{2}/, null, {
+        message: 'dob must be YYYY-MM-DD'
+    })
     dob: string;
 
     @IsString()
