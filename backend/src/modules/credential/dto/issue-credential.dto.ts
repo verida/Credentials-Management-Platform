6e91928417a57da3b78a06239f393153bf795cfb
Chris Were <chris@mozzler.com.au>
2020-07-10 21:16:15 +0930

    Feat: Add regex validator to dob

--- backend/src/modules/credential/dto/issue-credential.dto.ts
+++ backend/src/modules/credential/dto/issue-credential.dto.ts
@@ -1,9 +1,10 @@
-import { IsObject, IsString, Length, IsNotEmpty, IsDefined, Contains } from 'class-validator';
+import { IsObject, IsString, Length, IsNotEmpty, Matches } from 'class-validator';
 
 export class IssueCredentialDto {
     @IsString()
-    @Length(8)
+    @Length(8,8)
     @IsNotEmpty()
+    @Matches(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)
     dob: string;
 
     @IsString()
