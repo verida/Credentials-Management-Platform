6d7849f226c5ec96c8c9a45ac7815083cbe2dc05
Michael Chinweike <infinity_michael@yahoo.com>
2021-10-15 07:20:48 +0100

    feat: Integrated new Verida Client SDK - integrated with @verida/3id-utils-node - @verida/account-node - @verida/client-ts - ufor creating new issuer - updated typescript to v4.4.4  for the  backend project #1

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import {Body, Headers, Injectable} from '@nestjs/common';
+/* eslint-disable @typescript-eslint/camelcase */
+import { Body, Headers, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 import { AdminService } from '../admin/admin.service';
@@ -10,49 +11,52 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private adminService: AdminService,
-        private userService: UserService,
-        private jwtService: JwtService
-    ) {}
+  constructor(
+    private adminService: AdminService,
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-    async find (admin: boolean, email): Promise<UserIdentity|AdminIdentity> {
-        const service = (admin && this.adminService) || this.userService;
-        let user = await service.findOne(email) as (UserIdentity|AdminIdentity);
-        if (user) {
-            const data = user.toObject ? user.toObject() : user;
-            data['isAdmin'] = Boolean(admin);
-            user = data;
-        }
-
-        return user;
+  async find(admin: boolean, email): Promise<UserIdentity | AdminIdentity> {
+    const service = (admin && this.adminService) || this.userService;
+    let user = (await service.findOne(email)) as UserIdentity | AdminIdentity;
+    if (user) {
+      const data = user.toObject ? user.toObject() : user;
+      data['isAdmin'] = Boolean(admin);
+      user = data as UserIdentity;
     }
 
-    async validateUser(@Body() identity: Identity) {
-        const user = await this.find(identity.isAdmin, identity.email);
-        return user && bcrypt.compareSync(identity.password, user.passwordHash);
-    }
+    return user;
+  }
 
-    async userByToken(@Headers() headers): Promise<UserIdentity|AdminIdentity> {
-        const token = headers.authorization.replace('Bearer ', '');
-        const data = await this.jwtService.verifyAsync(token);
-        return this.find(data.isAdmin, data.email);
-    }
+  async validateUser(@Body() identity: Identity) {
+    const user = await this.find(identity.isAdmin, identity.email);
+    return user && bcrypt.compareSync(identity.password, user.passwordHash);
+  }
 
-    async login(@Body() identity) {
-        const { __v, passwordHash, ...user } = await this.find(identity.isAdmin, identity.email);
-        return {
-            user,
-            access_token: this.jwtService.sign({
-                email: user.email,
-                sub: user._id,
-                isAdmin: identity.isAdmin
-            }),
-        };
-    }
+  async userByToken(@Headers() headers): Promise<UserIdentity | AdminIdentity> {
+    const token = headers.authorization.replace('Bearer ', '');
+    const data = await this.jwtService.verifyAsync(token);
+    return this.find(data.isAdmin, data.email);
+  }
 
-    async restore(@Headers() headers) {
-        const { __v, passwordHash, ...user } = await this.userByToken(headers);
-        return { ...user }
-    }
+  async login(@Body() identity) {
+    const { __v, passwordHash, ...user } = await this.find(
+      identity.isAdmin,
+      identity.email,
+    );
+    return {
+      user,
+      access_token: this.jwtService.sign({
+        email: user.email,
+        sub: user._id,
+        isAdmin: identity.isAdmin,
+      }),
+    };
+  }
+
+  async restore(@Headers() headers) {
+    const { __v, passwordHash, ...user } = await this.userByToken(headers);
+    return { ...user };
+  }
 }
