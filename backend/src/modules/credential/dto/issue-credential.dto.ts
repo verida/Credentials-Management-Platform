fe86c535ecee76b816fc79215e9d6951fbf846e1
aurelticot <aurelticot.dev@gmail.com>
2022-06-03 16:34:54 +0200

    turn dob property in dto optional

--- backend/src/modules/credential/dto/issue-credential.dto.ts
+++ backend/src/modules/credential/dto/issue-credential.dto.ts
@@ -3,13 +3,14 @@ import {
   IsString,
   Length,
   IsNotEmpty,
+  IsOptional,
   Matches,
 } from 'class-validator';
 
 export class IssueCredentialDto {
   @IsString()
   @Length(10, 10)
-  @IsNotEmpty()
+  @IsOptional()
   @Matches(/[0-9]{4}-[0-9]{2}-[0-9]{2}/, null, {
     message: 'Date of Birth must be YYYY-MM-DD',
   })
