9aa7578e2d5e4a8a4ae5f72618a7c6adb75fa414
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-06-22 13:45:03 +0100

    feat: Handle Backend error - Handle errors with appropriate error message when issuing credentials Closes #86

--- backend/src/modules/credential/credential.controller.ts
+++ backend/src/modules/credential/credential.controller.ts
@@ -6,6 +6,8 @@ import {
   UseGuards,
   Req,
   Headers,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { validate } from 'class-validator';
 import { CredentialService } from './credential.service';
@@ -26,13 +28,14 @@ export class CredentialController {
     private userService: UserService,
     private issuerService: IssuerService,
     private authService: AuthService,
-  ) {}
+  ) { }
 
   @Post('issue')
   async issue(
     @Body() data: IssueCredentialDto,
     @Req() request,
   ): Promise<object> {
+
     await validate(data).then(errors => {
       // errors is an array of validation errors
       if (errors.length > 0) {
@@ -42,7 +45,17 @@ export class CredentialController {
 
     const user = await this.userService.findOneById(request.user.userId);
     const issuer = await this.issuerService.findOne(user.issuerId);
-    return this.credentialService.issue(issuer, data);
+    const credential = await this.credentialService.issue(issuer, data) as any;
+
+    if (credential.error && credential.error.message) {
+      throw new HttpException({
+        status: HttpStatus.BAD_REQUEST,
+        error: credential.error.message,
+      }, HttpStatus.BAD_REQUEST);
+    }
+
+
+    return credential
   }
 
   @Get()
