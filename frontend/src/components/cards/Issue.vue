471ba01f2b5102ea5deb77363a13309bfc3a7c56
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-08-11 23:52:25 +0100

    feat(frontend): Avatar upload - Upload avatar when creating an issuer - Update an issuer avatar and name #47

--- frontend/src/components/cards/Issue.vue
+++ frontend/src/components/cards/Issue.vue
@@ -1,14 +1,39 @@
 <template>
-  <v-card class="dashboard-card blue">
-    <h4 class="font-weight-regular white--text mb-4 text-capitalize">
-      {{ issue.name }}
-    </h4>
+  <v-card class="mx-auto my-2" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">Issuer Profile</div>
+        <v-list-item-title class="text-h5 mb-1"
+          >{{ issue.name }}
+        </v-list-item-title>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey">
+        <v-img :src="issue.avatarUri"></v-img>
+      </v-list-item-avatar>
+    </v-list-item>
+    <v-card-actions>
+      <v-btn outlined text @click="openEditProfileModal"> Edit Profile </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
-
 <script>
+import { createNamespacedHelpers } from "vuex";
+const { mapMutations: mapIssuerMutation } = createNamespacedHelpers("issuer");
+const { mapMutations: mapSystemMutations } = createNamespacedHelpers("system");
 export default {
   name: "Issue",
   props: ["issue"],
+  methods: {
+    ...mapSystemMutations(["openModal"]),
+    ...mapIssuerMutation(["setIssuerById"]),
+
+    openEditProfileModal() {
+      this.setIssuerById(this.issue);
+      this.openModal({
+        type: "UpdateIssuerProfile",
+      });
+    },
+  },
 };
 </script>
--- frontend/src/views/Issuers.vue
+++ frontend/src/views/Issuers.vue
@@ -1,20 +1,20 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <v-btn color="primary" outlined rounded @click="open">
-        New Issuer
-      </v-btn>
+    <v-row justify="center my-5">
+      <v-btn color="primary" outlined @click="open"> New Issuer </v-btn>
     </v-row>
     <v-row justify="center" class="mt-5" v-if="!list || !list.length">
       There are no issuers yet
     </v-row>
-    <v-row justify="center" v-else>
-      <issue
-        class="mx-2 my-5"
-        v-for="issuer in list"
+    <v-row class="my-5" else justify="center">
+      <v-col
         :key="`issue-${issuer._id}`"
-        :issue="issuer"
-      />
+        v-for="issuer in list"
+        class="pa-0 mx-2"
+        cols="3"
+      >
+        <issue :issue="issuer" />
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -23,17 +23,15 @@
 import Issue from "../components/cards/Issue";
 
 import { createNamespacedHelpers } from "vuex";
-const {
-  mapState: mapIssuerState,
-  mapActions: mapIssuerActions
-} = createNamespacedHelpers("issuer");
+const { mapState: mapIssuerState, mapActions: mapIssuerActions } =
+  createNamespacedHelpers("issuer");
 const { mapMutations: mapSystemMutations } = createNamespacedHelpers("system");
 
 export default {
   name: "Issuers",
   components: { Issue },
   computed: {
-    ...mapIssuerState(["list"])
+    ...mapIssuerState(["list"]),
   },
   async beforeMount() {
     await this.fetchIssuers();
@@ -43,9 +41,9 @@ export default {
     ...mapIssuerActions(["fetchIssuers"]),
     open() {
       this.openModal({
-        type: "NewCredentialIssuerDialog"
+        type: "NewCredentialIssuerDialog",
       });
-    }
-  }
+    },
+  },
 };
 </script>
