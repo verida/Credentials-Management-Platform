4d57bf7b8f6a59faca35638a373eb481cf4733dc
aurelticot <aurelticot.dev@gmail.com>
2022-09-14 12:28:01 +1000

    clean env var and set up config service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { UserModule } from './modules/user/user.module';
@@ -7,24 +8,39 @@ import { AdminModule } from './modules/admin/admin.module';
 import { CredentialModule } from './modules/credential/credential.module';
 import { IssuerModule } from './modules/issuer/issuer.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { SchemaModule } from "./modules/schema/schema.module";
-
+import { SchemaModule } from './modules/schema/schema.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        ENABLE_CORS: Joi.boolean().default(true),
+        CORS_HOSTS: Joi.string().default(''),
+        DB_URL: Joi.string().required(),
+        SALT: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        VERIDA_ENVIRONMENT: Joi.string().default('testnet'),
+        VERIDA_APP_NAME: Joi.string().required(),
+        VERIDA_TESTNET_DEFAULT_SERVER: Joi.string().default(
+          'https://db.testnet.verida.io:5002/',
+        ),
+      }),
     }),
     MongooseModule.forRoot(process.env.DB_URL, {
       useUnifiedTopology: true,
-      useNewUrlParser: true
+      useNewUrlParser: true,
     }),
     SchemaModule,
     UserModule,
     AdminModule,
     CredentialModule,
     IssuerModule,
-    AuthModule
+    AuthModule,
   ],
 })
 export class AppModule {}
